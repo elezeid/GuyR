b0ef07be25b3d9973e8fdcc5493fbafb71f4ac93
elezeid <162516710+elezeid@users.noreply.github.com>
2025-01-21 15:38:45 -0500

    First pass

---
+++ Scripts/ScriptProcessors/Preset/Interface.js
@@ -0,0 +1,48 @@
+Content.makeFrontInterface(600, 600);
+
+
+const var ConvolutionReverb1 = Synth.getAudioSampleProcessor("Convolution Reverb1");
+
+const irs = Engine.loadAudioFilesIntoPool()
+
+//cmbIR
+const cmbIR = Content.getComponent("cmbIR");
+
+
+inline function oncmbIRControl(component, value)
+{
+	if (value > o)
+		ConvolutionReverb1.setFile(irs[value - 1]);
+};
+
+Content.getComponent("cmbIR").setControlCallback(oncmbIRControl);
+
+cmbIR.set("items", "");
+
+for (x in irs)
+	cmbIR.addItem(x.replace("{PROJECT_FOLDER}").replace(".wav"));
+
+
+
+
+function onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+	
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
