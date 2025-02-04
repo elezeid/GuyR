e79d6c4160ba8b688394ee54d228409549cc17e4
elezeid <162516710+elezeid@users.noreply.github.com>
2025-02-04 13:08:35 -0500

    delay switching
    added a pause between switching presets to prevent crashes, hopefully

---
+++ Scripts/ScriptProcessors/ConvolutionwithAIcode/Interface.js
@@ -0,0 +1,53 @@
+Content.makeFrontInterface(600, 600);
+
+
+const var ConvolutionReverb1 = Synth.getAudioSampleProcessor("Convolution Reverb1");
+
+const irs = Engine.loadAudioFilesIntoPool();
+
+//cmbIR
+const cmbIR = Content.getComponent("cmbIR");
+
+
+inline function oncmbIRControl(component, value)
+{
+	if (value > 0)
+		ConvolutionReverb1.setBypassed(true);   // Disable convolution temporarily
+		ConvolutionReverb1.setFile(irs[value - 1]);  // Load new IR
+		ConvolutionReverb1.setBypassed(false);  // Re-enable convolution
+
+};
+
+Content.getComponent("cmbIR").setControlCallback(oncmbIRControl);
+
+cmbIR.set("items", "");
+
+for (x in irs)
+	cmbIR.addItem(x.replace("{PROJECT_FOLDER}").replace(".wav"));
+	
+include("ZoomHandler.js");
+
+
+
+
+function onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+	
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
